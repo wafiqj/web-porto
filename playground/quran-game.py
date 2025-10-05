d9babcafcffb3168d668c787f32a43b708be7626
wafiqj <jaisyurrahmanwafiq@gmail.com>
2025-10-05 12:25:44 +0700

    minor revise

--- playground/quran-game.py
+++ playground/quran-game.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.markdown("# 📖 Quran Game :blue-badge[Coming Soon]")
-st.markdown("Tags : :blue-badge[Speech Recognition], :blue-badge[Speech-to-text], :blue-badge[Fuzzy Analysis]")
+st.markdown("Tags : :blue-badge[Speech Recognition], :blue-badge[Fuzzy Analysis]")
 st.info("This playground is coming soon. Stay tuned for updates!")
